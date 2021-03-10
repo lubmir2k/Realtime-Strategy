addb9d63fe9594c859d86b3c4e0da7d468acbdd1
lubmir2k <lubomir.mitana@gmail.com>
2021-03-10 11:33:00 +0100

    Unit selection & movement

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.Events;
+
+public class Unit : NetworkBehaviour
+{
+    [SerializeField] private UnitMovement unitMovement = null;
+    [SerializeField] private UnityEvent onSelected = null;
+    [SerializeField] private UnityEvent onDeselected = null;
+
+    public UnitMovement GetUnitMovement()
+    {
+        return unitMovement;
+    }
+
+    #region Client
+
+    [Client]
+   public void Select()
+    {
+        if(!hasAuthority) { return; }
+
+        onSelected?.Invoke();
+    }
+
+    [Client]
+    public void Deselect()
+    {
+        if (!hasAuthority) { return; }
+
+        onDeselected?.Invoke();
+    }
+
+    #endregion
+}
